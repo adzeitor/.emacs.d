b3f539292a769f1bcba4f1d8e1c73e064c09f781
Alexander Voikov <alex-voikov@yandex.ru>
2012-06-09 16:45:52 +0600

    Refactoring

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 (let ((default-directory "~/.emacs.d/plugins"))
   (normal-top-level-add-subdirs-to-load-path))
 
+;;(let ((default-directory "~/.emacs.d/elpa"))
+;;  (normal-top-level-add-subdirs-to-load-path))
+
+
 (setq inhibit-splash-screen t)
 
 (global-hl-line-mode 0)
@@ -9,10 +13,12 @@
 ;; Show line-number in the mode line
 (line-number-mode 1)
 
+(global-linum-mode t)
+
 ;; Show column-number in the mode line
 (column-number-mode 1)
 
-(setq scroll-conservatively 50)
+(setq scroll-conservatively 10000)
 (setq scroll-preserve-screen-position 't)
 (setq scroll-margin 10)
 
@@ -23,27 +29,27 @@
 
 ;;(setq js-js-switch-tab 1)
 
-(defmacro smart-tabs-advice (function offset)
-  `(progn
-     (defvaralias ',offset 'tab-width)
-     (defadvice ,function (around smart-tabs activate)
-       (cond
-	(indent-tabs-mode
-	 (save-excursion
-	   (beginning-of-line)
-	   (while (looking-at "\t*\\( +\\)\t+")
-	     (replace-match "" nil nil nil 1)))
-	 (setq tab-width tab-width)
-	 (let ((tab-width fill-column)
-	       (,offset fill-column)
-	       (wstart (window-start)))
-	   (unwind-protect
-	       (progn ad-do-it)
-	     (set-window-start (selected-window) wstart))))
-	(t
-	 ad-do-it)))))
+;; (defmacro smart-tabs-advice (function offset)
+;;   `(progn
+;;      (defvaralias ',offset 'tab-width)
+;;      (defadvice ,function (around smart-tabs activate)
+;;        (cond
+;; 	(indent-tabs-mode
+;; 	 (save-excursion
+;; 	   (beginning-of-line)
+;; 	   (while (looking-at "\t*\\( +\\)\t+")
+;; 	     (replace-match "" nil nil nil 1)))
+;; 	 (setq tab-width tab-width)
+;; 	 (let ((tab-width fill-column)
+;; 	       (,offset fill-column)
+;; 	       (wstart (window-start)))
+;; 	   (unwind-protect
+;; 	       (progn ad-do-it)
+;; 	     (set-window-start (selected-window) wstart))))
+;; 	(t
+;; 	 ad-do-it)))))
 
-(smart-tabs-advice js2-indent-line js2-basic-offset)
+;; (smart-tabs-advice js2-indent-line js2-basic-offset)
 
 ;; (smart-tabs-advice erlang-indent-line erlang-indent-level)
 ;; (setq erlang-indent-tabs-mode t)
@@ -61,228 +67,43 @@
 (setq visible-bell t)
 
 
-(eval-when-compile
-  (require 'color-theme))
 
-(defun color-theme-tangotango ()
-  "A color theme based on Tango Palette colors."
-  ;; Color codes :
-  ;; - blue :       "dodger blue"
-  ;; - yellow :     "#edd400"
-  ;; - green :      "#6ac214"
-  ;; - orange/red : "tomato"
-  (interactive)
-  (color-theme-install
-   '(color-theme-tangotango
-     ((background-color . "#2e3434")
-      (background-mode . dark)
-      (border-color . "#888a85")
-      (foreground-color . "#eeeeec")
-      (mouse-color . "#8ae234"))
-     ((help-highlight-face . underline)
-      (ibuffer-dired-buffer-face . font-lock-function-name-face)
-      (ibuffer-help-buffer-face . font-lock-comment-face)
-      (ibuffer-hidden-buffer-face . font-lock-warning-face)
-      (ibuffer-occur-match-face . font-lock-warning-face)
-      (ibuffer-read-only-buffer-face . font-lock-type-face)
-      (ibuffer-special-buffer-face . font-lock-keyword-face)
-      (ibuffer-title-face . font-lock-type-face))
-     (cursor ((t (:background "#fce94f" :foreground "#222222"))))
-     (highlight ((t (:background "brown4" :foreground nil))))
-     (border ((t (:background "#888a85"))))
-     (fringe ((t (:background "grey10"))))
-     (mode-line ((t (:foreground "#bbbbbc" :background "#222222" :box (:line-width 1 :color nil :style released-button)))))
-     (mode-line-inactive ((t (:foreground "#bbbbbc" :background "#555753"))))
-     (mode-line-buffer-id ((t (:bold t :foreground "orange" :background nil))))
-     (region ((t (:background "dark slate blue"))))
-     (link ((t (:underline t :foreground "dodger blue"))))
-     (custom-link ((t (:inherit 'link))))
-     (match ((t (:bold t :background "#e9b96e" :foreground "#2e3436"))))
-     (tool-tips ((t (:inherit 'variable-pitch :foreground "black" :background "lightyellow"))))
-     (tooltip ((t (:inherit 'variable-pitch :foreground "black" :background "lightyellow"))))
-     (bold ((t (:bold t :underline nil :background nil))))
-     (italic ((t (:italic t :underline nil :background nil))))
-     (font-lock-builtin-face ((t (:foreground "#729fcf"))))
-     (font-lock-comment-face ((t (:foreground "#888a85"))))
-     (font-lock-constant-face ((t (:foreground "#8ae234"))))
-     (font-lock-doc-face ((t (:foreground "#888a85"))))
-     (font-lock-keyword-face ((t (:foreground "#729fcf" :bold t))))
-     (font-lock-string-face ((t (:foreground "#ad7fa8" :italic t))))
-     (font-lock-type-face ((t (:foreground "#8ae234" :bold t))))
-     (font-lock-variable-name-face ((t (:foreground "tomato"))))
-     (font-lock-warning-face ((t (:bold t :foreground "#f57900"))))
-     (font-lock-function-name-face ((t (:foreground "#edd400" :bold t))))
-     (comint-highlight-input ((t (:italic t :bold t))))
-     (comint-highlight-prompt ((t (:foreground "#8ae234"))))
-     (isearch ((t (:background "#f57900" :foreground "#2e3436"))))
-     (isearch-lazy-highlight-face ((t (:foreground "#2e3436" :background "#e9b96e"))))
-     (show-paren-match-face ((t (:foreground "#2e3436" :background "#73d216"))))
-     (show-paren-mismatch-face ((t (:background "#ad7fa8" :foreground "#2e3436"))))
-     (info-xref ((t (:foreground "#729fcf"))))
-     (info-xref-visited ((t (:foreground "#ad7fa8"))))
-     (diary-face ((t (:bold t :foreground "IndianRed"))))
-     (eshell-ls-clutter-face ((t (:bold t :foreground "DimGray"))))
-     (eshell-ls-executable-face ((t (:bold t :foreground "Coral"))))
-     (eshell-ls-missing-face ((t (:bold t :foreground "black"))))
-     (eshell-ls-special-face ((t (:bold t :foreground "Gold"))))
-     (eshell-ls-symlink-face ((t (:bold t :foreground "White"))))
-     (widget-button ((t (:bold t))))
-     (widget-mouse-face ((t (:bold t :foreground "white" :background "brown4"))))
-     (widget-field ((t (:foreground "orange" :background "gray30"))))
-     (widget-single-line-field ((t (:foreground "orange" :background "gray30"))))
-     (custom-group-tag ((t (:bold t :foreground "#edd400" :height 1.3))))
-     (custom-variable-tag ((t (:bold t :foreground "#edd400" :height 1.1))))
-     (custom-face-tag ((t (:bold t :foreground "#edd400" :height 1.1))))
-     (custom-state-face ((t (:foreground "#729fcf"))))
-     (custom-button  ((t (:box (:line-width 1 :style released-button) :background "grey50" :foreground "black"))))
-     (custom-variable-button ((t (:inherit 'custom-button))))
-     (custom-button-mouse  ((t (:inherit 'custom-button :background "grey60"))))
-     (custom-button-unraised  ((t (:background "grey50" :foreground "black"))))
-     (custom-button-mouse-unraised  ((t (:inherit 'custom-button-unraised :background "grey60"))))
-     (custom-button-pressed  ((t (:inherit 'custom-button :box (:style pressed-button)))))
-     (custom-button-mouse-pressed-unraised  ((t (:inherit 'custom-button-unraised :background "grey60"))))
-     (custom-documentation ((t (:italic t))))
-     (message-cited-text ((t (:foreground "#edd400"))))
-     (gnus-cite-face-1 ((t (:foreground "#ad7fa8"))))
-     (gnus-cite-face-2 ((t (:foreground "sienna4"))))
-     (gnus-cite-face-3 ((t (:foreground "khaki4"))))
-     (gnus-cite-face-4 ((t (:foreground "PaleTurquoise4"))))
-     (gnus-group-mail-1-empty((t (:foreground "light cyan"))))
-     (gnus-group-mail-1((t (:bold t :foreground "light cyan"))))
-     (gnus-group-mail-2-empty((t (:foreground "turquoise"))))
-     (gnus-group-mail-2((t (:bold t :foreground "turquoise"))))
-     (gnus-group-mail-3-empty((t (:foreground "#729fcf"))))
-     (gnus-group-mail-3((t (:bold t :foreground "#edd400"))))
-     (gnus-group-mail-low-empty((t (:foreground "dodger blue"))))
-     (gnus-group-mail-low((t (:bold t :foreground "dodger blue"))))
-     (gnus-group-news-1-empty((t (:foreground "light cyan"))))
-     (gnus-group-news-1((t (:bold t :foreground "light cyan"))))
-     (gnus-group-news-2-empty((t (:foreground "turquoise"))))
-     (gnus-group-news-2((t (:bold t :foreground "turquoise"))))
-     (gnus-group-news-3-empty((t (:foreground "#729fcf"))))
-     (gnus-group-news-3((t (:bold t :foreground "#edd400"))))
-     (gnus-group-news-low-empty((t (:foreground "dodger blue"))))
-     (gnus-group-news-low((t (:bold t :foreground "dodger blue"))))
-     (gnus-header-name ((t (:bold t :foreground "#729fcf"))))
-     (gnus-header-from ((t (:bold t :foreground "#edd400"))))
-     (gnus-header-subject ((t (:foreground "#edd400"))))
-     (gnus-header-content ((t (:italic t :foreground "#8ae234"))))
-     (gnus-header-newsgroups((t (:italic t :bold t :foreground "LightSkyBlue3"))))
-     (gnus-signature((t (:italic t :foreground "dark grey"))))
-     (gnus-summary-cancelled((t (:background "black" :foreground "yellow"))))
-     (gnus-summary-high-ancient((t (:bold t :foreground "rotal blue"))))
-     (gnus-summary-high-read((t (:bold t :foreground "lime green"))))
-     (gnus-summary-high-ticked((t (:bold t :foreground "tomato"))))
-     (gnus-summary-high-unread((t (:bold t :foreground "white"))))
-     (gnus-summary-low-ancient((t (:italic t :foreground "lime green"))))
-     (gnus-summary-low-read((t (:italic t :foreground "royal blue"))))
-     (gnus-summary-low-ticked((t (:italic t :foreground "dark red"))))
-     (gnus-summary-low-unread((t (:italic t :foreground "white"))))
-     (gnus-summary-normal-ancient((t (:foreground "royal blue"))))
-     (gnus-summary-normal-read((t (:foreground "lime green"))))
-     (gnus-summary-normal-ticked((t (:foreground "indian red"))))
-     (gnus-summary-normal-unread((t (:foreground "white"))))
-     (gnus-summary-selected ((t (:background "brown4" :foreground "white"))))
-     (message-header-name((t (:foreground "tomato"))))
-     (message-header-newsgroups((t (:italic t :bold t :foreground "LightSkyBlue3"))))
-     (message-header-other((t (:foreground "LightSkyBlue3"))))
-     (message-header-xheader((t (:foreground "DodgerBlue3"))))
-     (message-header-subject ((t (:foreground "white"))))
-     (message-header-to ((t (:foreground "white"))))
-     (message-header-cc ((t (:foreground "white"))))
-     (org-hide ((t (:foreground "#2e3436"))))
-     (org-level-1 ((t (:bold t :foreground "dodger blue" :height 1.5))))
-     (org-level-2 ((t (:bold nil :foreground "#edd400" :height 1.2))))
-     (org-level-3 ((t (:bold t :foreground "#6ac214" :height 1.0))))
-     (org-level-4 ((t (:bold nil :foreground "tomato" :height 1.0))))
-     (org-date ((t (:underline t :foreground "magenta3"))))
-     (org-footnote  ((t (:underline t :foreground "magenta3"))))
-     (org-link ((t (:foreground "skyblue2" :background "#2e3436"))))
-     (org-special-keyword ((t (:foreground "brown"))))
-     (org-verbatim ((t (:foreground "#eeeeec" :underline t :slant italic))))
-     (org-block ((t (:foreground "#bbbbbc"))))
-     (org-quote ((t (:inherit org-block :slant italic))))
-     (org-verse ((t (:inherit org-block :slant italic))))
-     (org-todo ((t (:bold t :foreground "Red"))))
-     (org-done ((t (:bold t :foreground "ForestGreen"))))
-     (org-agenda-structure ((t (:weight bold :foreground "tomato"))))
-     (org-agenda-date ((t (:foreground "#6ac214"))))
-     (org-agenda-date-weekend ((t (:weight normal :foreground "dodger blue"))))
-     (org-agenda-date-today ((t (:weight bold :foreground "#edd400"))))
-     (anything-header ((t (:bold t :background "grey15" :foreground "#edd400"))))
-     (anything-candidate-number ((t (:background "#f57900" :foreground "black"))))
-     (ess-jb-comment-face ((t (:background "#2e3436" :foreground "firebrick" :slant italic))))
-     (ess-jb-hide-face ((t (:background "#2e3436" :foreground "#243436"))))
-     (ess-jb-h1-face ((t (:height 1.6 :foreground "dodger blue" :slant normal))))
-     (ess-jb-h2-face ((t (:height 1.4 :foreground "#6ac214" :slant normal))))
-     (ess-jb-h3-face ((t (:height 1.2 :foreground "#edd400" :slant normal))))
-     (ecb-default-highlight-face ((t (:background "#729fcf"))))
-     (ecb-tag-header-face ((t (:background "#f57900"))))
-     (magit-header ((t (:foreground "#edd400"))))
-     (magit-diff-add ((t (:foreground "#729fcf"))))
-     (magit-item-highlight ((t (:weight extra-bold :inverse-video t))))
-     (diff-header ((t (:background "gray30"))))
-     (diff-index ((t (:foreground "#edd400" :bold t))))
-     (diff-file-header ((t (:foreground "#eeeeec" :bold t))))
-     (diff-hunk-header ((t (:foreground "#edd400"))))
-     (diff-added ((t (:foreground "#8ae234"))))
-     (diff-removed ((t (:foreground "#f57900"))))
-     (diff-context ((t (:foreground "#888a85"))))
-     (diff-refine-change ((t (:bold t :background "gray30"))))
-     (ediff-current-diff-A ((t (:background "#555753"))))
-     (ediff-current-diff-Ancestor ((t (:background "#555753"))))
-     (ediff-current-diff-B ((t (:background "#555753"))))
-     (ediff-current-diff-C ((t (:background "#555753"))))
-     (ediff-even-diff-A ((t (:background "gray30"))))
-     (ediff-even-diff-Ancestor ((t (:background "gray30"))))
-     (ediff-even-diff-B ((t (:background "gray30"))))
-     (ediff-even-diff-C ((t (:background "gray30"))))
-     (ediff-odd-diff-A ((t (:background "gray30"))))
-     (ediff-odd-diff-Ancestor ((t (:background "gray30"))))
-     (ediff-odd-diff-B ((t (:background "gray30"))))
-     (ediff-odd-diff-C ((t (:background "gray30"))))
-     (ediff-fine-diff-A ((t (:background "#222222"))))
-     (ediff-fine-diff-Ancestor ((t (:background "#222222"))))
-     (ediff-fine-diff-B ((t (:background "#222222"))))
-     (ediff-fine-diff-C ((t (:background "#222222"))))
-     (minibuffer-prompt ((t (:foreground "#729fcf" :bold t))))
-     (mumamo-background-chunk-major ((t (:background nil))))
-     (mumamo-background-chunk-submode1 ((t (:background "#2E3440"))))
-     (mumamo-background-chunk-submode2 ((t (:background "#2E4034"))))
-     (mumamo-background-chunk-submode3 ((t (:background "#343434"))))
-)))
-
-
-(require 'color-theme)
-(color-theme-initialize)
-(setq color-theme-is-global t)
-(setq color-theme-is-cumulative t)
-(setq color-theme-load-all-themes nil)
+;;(require 'color-theme)
+;;(color-theme-initialize)
+;;(setq color-theme-is-global t)
+;;(setq color-theme-is-cumulative t)
+;;(setq color-theme-load-all-themes nil)
 
 ;(color-theme-black)
 ;(color-theme-simple-1)
-(color-theme-tangotango)
+;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn-emacs")
 
+;; (load-file "~/.emacs.d/twilight-emacs/color-theme-twilight.el")
+(load-theme 'wombat t)
 
 (require 'whitespace)
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (setq whitespace-line-column 80)
 (global-whitespace-mode t)
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
-;(set-face-attribute 'default nil :font "Terminus-14")
-(set-face-attribute 'default nil :font "Monospace-14")
-;(set-face-attribute 'default nil :font "PragmataPro-14")
-;(set-face-attribute 'default nil :font "Monofur-16")
+(if (window-system)
+;;    (set-face-attribute 'default nil :font "Monofur-14")
 
-(custom-set-faces
- '(my-tab-face            ((((class color)) (:background "grey10"))) t)
- '(my-trailing-space-face ((((class color)) (:background "gray10"))) t)
- '(my-long-line-face ((((class color)) (:background "gray10"))) t))
+;(set-face-attribute 'default nil :font "Terminus-12")
+;(set-face-attribute 'default nil :font "Inconsolata-18")
+;(set-face-attribute 'default nil :font "Monospace-14")
+(set-face-attribute 'default nil :font "PragmataPro-18")
+;;(set-face-attribute 'default nil :font "Anonymous Pro-18")
+;;(set-face-attribute 'default nil :font "Unifont-12")
+)
 
-(add-to-list 'load-path "~/.emacs.d/nyan-mode/")
+;; nyan-mode
 (require 'nyan-mode)
-
 (nyan-mode)
+;;
+(require 'hpaste)
+
 (nyan-start-animation)
 (setq display-time-12hr-format t)
 (display-time)
@@ -333,31 +154,83 @@
 ;; 	    (when (not (memq major-mode '(makefile-mode makefile-bsdmake-mode)))
 ;; 	      (whitespace-cleanup))))
 
-(add-hook 'before-save-hook 'whitespace-cleanup)
 
-
-(global-linum-mode t)
 
 (global-set-key "" (quote comment-or-uncomment-region))
-;(global-set-key "\C-[tab]" 'other-window)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+(require 'cl)
+(defun hexcolour-luminance (color)
+  "Calculate the luminance of a color string (e.g. \"#ffaa00\", \"blue\").
+  This is 0.3 red + 0.59 green + 0.11 blue and always between 0 and 255."
+  (let* ((values (x-color-values color))
+	 (r (car values))
+	 (g (cadr values))
+	 (b (caddr values)))
+    (floor (+ (* .3 r) (* .59 g) (* .11 b)) 256)))
+
+(defun hexcolour-add-to-font-lock ()
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   `((,(concat "#[0-9a-fA-F]\\{3\\}[0-9a-fA-F]\\{3\\}?\\|"
+	       (regexp-opt (x-defined-colors) 'words))
+      (0 (let ((colour (match-string-no-properties 0)))
+	   (put-text-property
+	    (match-beginning 0) (match-end 0)
+	    'face `((:foreground ,(if (> 128.0 (hexcolour-luminance colour))
+				      "white" "black"))
+		    (:background ,colour)))))))))
+
+;;(add-hook 'emacs-lisp-mode-hook 'hexcolour-add-to-font-lock)
+
+
+
+(require 'rainbow-delimiters)
+
+;;(require 'solarized-dark-theme)
+(global-rainbow-delimiters-mode 1)
+
+;;(require 'color-theme-tangotango)
+
+
+(show-paren-mode t)
+;;(setq show-paren-style 'expression)
+
+;;(color-theme-tangotango)
+;;(color-theme-zenburn)
+
+(ido-mode 1)
+
+(require 'haskell-mode)
+(add-hook 'haskell-mode-hook 'hexcolour-add-to-font-lock)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
 (setq haskell-font-lock-symbols 't)
 
-;; Change user info
-;; (setq rcirc-default-nick "adzeitor")
-;; (setq rcirc-default-user-name "adzeitor1")
-;; (setq rcirc-default-full-name "adzeitor2")
+;;(require 'color-theme)
+;;(if window-system
+  ;;  (color-theme-zenburn)
+   ;; (color-theme-hober))
 
-;; Join these channels at startup.
-;; (setq rcirc-startup-channels-alist
-;;       '(("\\.freenode\\.net$" "#hostel7" "#stark")))
 
-;; Connect to servers.
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(my-long-line-face ((((class color)) (:background "gray10"))) t)
+ '(my-tab-face ((((class color)) (:background "gray10"))) t)
+ '(my-trailing-space-face ((((class color)) (:background "gray10"))) t))
 
-(require 'rcirc)
-(add-to-list 'load-path "~/.emacs.d/")
-(require 'pastebin)
-;(rcirc); freenodaxsx
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" "21d9280256d9d3cf79cbcf62c3e7f3f243209e6251b215aede5026e0c5ad853f" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" "42c6dbce1655fed22ca050d6f4aa27fda388dc244058350741480fc53221ebe4" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default))))
